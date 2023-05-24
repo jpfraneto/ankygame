4c1f52b103d774bbdc6058d4692a8640f8d7d3a3
jpfraneto <jpfraneto@gmail.com>
2023-05-24 14:04:50 -0700

    added world page

---
+++ pages/world.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const World = () => {
+  return (
+    <div>
+      <h2>What is this world?</h2>
+      <p>Anky is you.</p>
+    </div>
+  );
+};
+
+export default World;
