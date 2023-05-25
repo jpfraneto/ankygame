ef69b33e7ecbd59001fa44c07531e4c9a6e6314b
jpfraneto <jpfraneto@gmail.com>
2023-05-24 17:58:01 -0700

    holy shit

--- components/StepsForGettingImage.js
+++ components/StepsForGettingImage.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import Button from './Button';
+import Button from '@component/components/Button';
 import axios from 'axios';
 
 const StepsForGettingImage = ({ text }) => {
+  const [step, setStep] = useState(1);
   const [copyText, setCopyText] = useState('');
   const [ouch, setOuch] = useState(false);
   const [mintOptions, setMintOptions] = useState(false);
+
   const createPromptFromText = async () => {
-    // HERE I NEED TO QUERY THE API OF OPENAI
-    const basicText = `You are in charge of crafting a prompt for midjourney, which is an image generator. I will send you now a block of text that was written by a human being as a stream of consciousness, and your goal is to distill the essence of that writing so that you can come up with a graphic description of how the human that wrote it looks. Please avoid direct references to the writer, the goal of the prompt is just to get a description of how the human that created it looks like. The first part of the prompt should say: \n
-        "https://s.mj.run/YLJMlMJbo70, The profile picture of a cartoon"... \n\n Come up with a conscise writing for this, and send it back to me so that I can prompt it to midjourney. \n Here is the block of text:`;
+    const basicText = `You are in charge of crafting a prompt for midjourney...`;
     const prompt = basicText + '   ' + `"${text}"`;
     pasteTextOnClipboard(prompt);
     setOuch(true);
@@ -18,64 +18,105 @@ const StepsForGettingImage = ({ text }) => {
   const pasteTextOnClipboard = async promptText => {
     await navigator.clipboard.writeText(promptText);
     setCopyText('The prompt was copied on your clipboard');
+    setStep(2);
   };
+
+  const nextStep = () => {
+    if (step < 5) {
+      setStep(step + 1);
+    }
+  };
+
+  const previousStep = () => {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  };
+
   return (
     <div>
-      {' '}
-      {mintOptions ? (
-        <div>Mint Info</div>
-      ) : (
+      <h4 className='text-5xl mb-64'>
+        To create a unique avatar based on what you wrote:
+      </h4>
+      {step === 1 && (
         <div>
-          <h4 className='text-2xl mb-2 '>
-            To create a unique avatar based on what you wrote:
-          </h4>
-          <ul>
-            <li className='mb-2'>
-              1. Get the prompt for chatgtp based on what you wrote by clicking
-              this button{' '}
-              {ouch ? (
-                <Button
-                  buttonText='Ouch! That hurted.'
-                  buttonAction={() => {}}
-                  buttonColor='bg-theredbtn'
-                />
-              ) : (
-                <Button
-                  buttonText='Click me.'
-                  buttonAction={createPromptFromText}
-                  buttonColor='bg-thegreenbtn'
-                />
-              )}
-            </li>
-            <li className='mb-2'>
-              2. Go to the chatgtp tab that is open right above here, and paste.
-              What is in your clipboard is the prompt for that exact chat
-              section of the app. What it sends you back, you have to copy that
-              and come back here.
-            </li>
-            <li className='mb-2'>
-              3. Now, with what you just copied, go to the discord app and send
-              the /imagine command to the midjourney bot and paste what you have
-              there inside the image command.
-            </li>
-            <li className='mb-2'>
-              4. Then, choose one of those four images, and that is the
-              representation of anky that will exist with you the rest of your
-              life. This is a gift, from me, to you. If you want to get the NFT,
-              send me an email to jpfraneto@gmail.com with your wallet address.
-              Just your wallet address of the ethereum network. That's all I
-              need. If not, any feedback that you can give me helps me make this
-              thing even more awesome.
-            </li>
-            <li className='mb-2'>Thank you.</li>
-            <li className='mb-2 '>jp</li>
+          <p>
+            1. Get the prompt for chatgtp based on what you wrote by clicking
+            this button
+          </p>
+          {ouch ? (
+            <Button
+              buttonText='Ouch! That hurted.'
+              buttonAction={() => {}}
+              buttonColor='bg-thered'
+            />
+          ) : (
+            <Button
+              buttonText='Click me.'
+              buttonAction={createPromptFromText}
+              buttonColor='bg-theorange border border-thewhite'
+            />
+          )}
+        </div>
+      )}
 
-            {mintOptions && (
-              <div>
-                <p>Here are the steps for minting the NFT</p>
-              </div>
-            )}
-          </ul>
+      {step === 2 && (
+        <div>
+          <p>
+            2. Go to the chatgtp tab that is open right above here, and paste.
+            What is in your clipboard is the prompt for that exact chat section
+            of the app. What it sends you back, you have to copy that and come
+            back here.
+          </p>
+        </div>
+      )}
+
+      {step === 3 && (
+        <div>
+          <p>
+            3. Now, with what you just copied, go to the discord app and send
+            the /imagine command to the midjourney bot and paste what you have
+            there inside the image command.
+          </p>
+        </div>
+      )}
+
+      {step === 4 && (
+        <div>
+          <p>
+            4. Then, choose one of those four images, and that is the
+            representation of anky that will exist with you the rest of your
+            life.
+          </p>
+        </div>
+      )}
+
+      {step === 5 && (
+        <div>
+          <p>
+            This is a gift, from me, to you. If you want to get the NFT, send me
+            an email to jpfraneto@gmail.com with your wallet address. Just your
+            wallet address of the ethereum network. That's all I need. If not,
+            any feedback that you can give me helps me make this thing even more
+            awesome.
+          </p>
+        </div>
+      )}
+
+      {step === 5 ? (
+        <p>Thank you, for being who you are. You are awesome.</p>
+      ) : (
+        <div className='flex justify-center mt-4 space-x-4'>
+          <Button
+            buttonText='Previous'
+            buttonAction={previousStep}
+            buttonColor='bg-theredbtn'
+          />
+          <Button
+            buttonText='Next'
+            buttonAction={nextStep}
+            buttonColor='bg-thegreenbtn'
+          />
         </div>
       )}
     </div>
