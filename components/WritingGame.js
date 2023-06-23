a5d1c3f37720ee85cbc01d559396f932ac9487b9
jpfraneto <jpfraneto@gmail.com>
2023-06-23 10:35:28 -0400

    aloja

--- components/WritingGame.js
+++ components/WritingGame.js
@@ -73,6 +73,9 @@ const WritingGame = ({ userPrompt }) => {
   useEffect(() => {
     if (isActive && !isDone) {
       intervalRef.current = setInterval(() => {
+        if (time === 180) {
+          finishRun();
+        }
         setTime(time => time + 1);
       }, 1000);
     } else if (!isActive && !isDone) {
--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ const pacifico = Pacifico({ weight: '400', subsets: ['latin'] });
 const GamePage = () => {
   return (
     <>
-      <WritingGame userPrompt='finish this sentence: "I can..."' />
+      <WritingGame userPrompt='What brings me joy?' />
 
       <div className='min-h-screen bg-theblack py-8'>
         <p
