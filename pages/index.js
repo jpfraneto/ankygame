22d07a10e325ee6ad706f6bc96554b57a3dc2e16
jpfraneto <jpfraneto@gmail.com>
2023-06-24 07:07:02 -0400

    aloja

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ const pacifico = Pacifico({ weight: '400', subsets: ['latin'] });
 const GamePage = () => {
   return (
     <>
-      <WritingGame userPrompt='What brings me joy?' />
+      <WritingGame userPrompt='What is compassion?' />
 
       <div className='min-h-screen bg-theblack py-8'>
         <p
