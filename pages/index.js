dc49fb3f8d1856b19c7efe9ee252e1319d30f3cf
jpfraneto <jpfraneto@gmail.com>
2023-06-25 07:53:40 -0400

    changed the image

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ const pacifico = Pacifico({ weight: '400', subsets: ['latin'] });
 const GamePage = () => {
   return (
     <>
-      <WritingGame userPrompt='What is compassion?' />
+      <WritingGame userPrompt='What is my truth?' />
 
       <div className='min-h-screen bg-theblack py-8'>
         <p
