88472495b491b61c400689fd2458b84cad35835d
jpfraneto <jpfraneto@gmail.com>
2023-06-30 06:51:36 -0400

    aloja

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ const pacifico = Pacifico({ weight: '400', subsets: ['latin'] });
 const GamePage = () => {
   return (
     <>
-      <WritingGame userPrompt='What is your religion?' />
+      <WritingGame userPrompt='Why web3?' />
 
       <div className='min-h-screen bg-theblack py-8'>
         <p
